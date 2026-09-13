2bd205644dd52afda7404925d6fef9ab88a69815
Pornpipat Kumlong <pornpipat.ku@ku.th>
2026-09-13 12:23:23 +0700

    [dev] add delete endpoint

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -63,4 +63,20 @@ app.MapPut("/api/todos/{id}", (int id, TodoUpdateDto dto) =>
     }
 
 });
+
+app.MapDelete("/api/todos/{id}", (int id) =>
+{
+   try
+    {
+        var todo = todos.FirstOrDefault(t => t.Id == id);
+        if (todo is null) return Results.NotFound();
+
+        todos.Remove(todo);
+        return Results.NoContent();
+    }
+    catch(Exception ex)
+    {
+        return Results.Problem(ex.Message);
+    }
+});
 app.Run();
